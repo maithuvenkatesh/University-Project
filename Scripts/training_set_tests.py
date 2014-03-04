45e66c24e84081e5d2d93ee0cdeab7fe99372fe5
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2014-03-04 22:11:13 +0000

    Tests for training set

---
+++ Scripts/training_set_tests.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pylab as pl
+from race_parser import RaceParserNoHandicaps
+from horse_parser_no_handicaps import HorseParserNoHandicaps
+from utilities import split_dataset
+
+def get_goings(horses):
+    going = set()
+    for r in races:
+        going.add(races[r].going)
+
+    return going
+
+def main():
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
